b28fcef1b5bd1b68a9d9d947474071db35940b91
FacundoDM-dev <delmedicofacundomartin@gmail.com>
2023-04-29 12:11:22 +0200

    commit-5

--- lab-css-spotify-clone-es-master/js/index.js
+++ lab-css-spotify-clone-es-master/js/index.js
@@ -1,22 +1,2 @@
-console.log("PracticandoJS");
 
-let comidaFavorita;
-
-comidaFavorita = "Asadito";
-
-console.log(comidaFavorita);
-
-comidaFavorita = "Raviolis"
-
-console.log(comidaFavorita);
-
-console.log(comidaFavorita, typeof comidaFavorita);
-
-comidaFavorita = 20;
-
-console.log(comidaFavorita, typeof comidaFavorita);
-
-const nombre = "Facundo"
-
-console.log(nombre);
 
---
+++ lab-js-basic-algorithms-es-master/js/index.js
@@ -0,0 +1,12 @@
+// Iteración 1: Nombres y entradas
+
+const hacker1 = ("Facundo")
+console.log(hacker1);
+const hacker2 = ("Diego")
+console.log(hacker2)
+
+// Iteración 2: Condicionales
+
+
+
+
