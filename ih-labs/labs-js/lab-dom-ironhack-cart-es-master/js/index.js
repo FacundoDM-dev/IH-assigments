f5ae9907c65a7bb9dbfdd33339f70ce1ead3bb10
FacundoDM-dev <delmedicofacundomartin@gmail.com>
2023-05-12 23:38:36 +0200

    commit-1

--- ih-labs/labs-js/lab-dom-ironhack-cart-es-master/js/index.js
+++ ih-labs/labs-js/lab-dom-ironhack-cart-es-master/js/index.js
@@ -1,40 +1,37 @@
 // ITERATION 1
 
 const updateSubtotal = (product) => {
-  const price = product.querySelector('.price span');
-  const quantity = product.querySelector('.quantity input');
-  const subtotal = product.querySelector('.subtotal span');
+  let price = product.querySelector('.price span');
+  let quantity = product.querySelector('.quantity input');
+  let subtotal = product.querySelector('.subtotal span');
 
-  let calculate = price.innerText * quantity.value;
-  let result = (subtotal.innerHTML = calculate);
+  const calculate = price.innerText * quantity.value;
+  const result = (subtotal.innerText = calculate);
 
   return result;
 };
 
 const calculateAll = () => {
-
   // ITERATION 2
-  
+
   const products = document.querySelectorAll('.product');
   let counter = 0;
   products.forEach((product) => {
-  counter += updateSubtotal(product);
-
+    counter += updateSubtotal(product);
   });
 
   // ITERATION 3
-  
-  let total = document.querySelector('#total-value span');
+
+  const total = document.querySelector('#total-value span');
   total.innerText = counter;
 };
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  let tbody = document.querySelector("tbody").parentNode
-  
+  const target = event.currentTarget.parentElement.parentElement;
+  target.parentElement.removeChild(target);
+  calculateAll(event)
 }
 
 // ITERATION 5
@@ -47,18 +44,14 @@ window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  const removeBtn = document.querySelectorAll(".btn-remove")
-  removeBtn.forEach(button => { 
-    button.addEventListener("click" , removeProduct)
+  const removeBtn = document.querySelectorAll('.btn-remove');
+  removeBtn.forEach((button) => {
+    button.addEventListener('click', removeProduct);
   });
-
   // for (let i = 0; i < removeBtn.length; i++) {
   //   const element = removeBtn[i];
-  //   element.addEventListener("click", removeProduct)S
+  //   element.addEventListener("click", removeProduct)
   // }
-  
-  
-
 
   //... your code goes here
 });
