2c4161b9c49bda60335dfd5eec7ccb330fe1e67f
FacundoDM-dev <delmedicofacundomartin@gmail.com>
2023-05-25 22:51:37 +0200

    commit-1

--- ih-projects/midterm-project-boiler/resources/js/index.js
+++ ih-projects/midterm-project-boiler/resources/js/index.js
@@ -22,3 +22,34 @@ let projectData = fetch("https://jsonplaceholder.typicode.com/posts")
     projectArray[2].querySelector("p").innerText = res[7].body;
   })
   .catch((err) => console.log(err));
+
+
+
+
+const d = document;
+const w = window;
+
+w.addEventListener("scroll", ()=>{
+    let button = document.querySelector(".scroll-top-btn")
+    let scrollTop = d.documentElement.scrollTop
+
+    if(scrollTop > 120){
+        button.classList.remove("hidden")
+    }else {
+        button.classList.add("hidden")
+    }
+})
+
+// d.addEventListener("click", (e)=>{
+//     let button = document.querySelector(".scroll-top-btn")
+//     if (e.target.matches(button)) {
+//         d.scrollTo({
+//             behavior: "smooth",
+//             top: 0,
+//             left: 0,
+//         })
+//     }
+// })
+
+
+
