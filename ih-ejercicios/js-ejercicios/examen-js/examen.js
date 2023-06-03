4764d261b42c20c0b8f7da6039af687f3e3b5048
FacundoDM-dev <delmedicofacundomartin@gmail.com>
2023-06-03 22:02:13 +0200

    commit-1

---
+++ ih-ejercicios/js-ejercicios/examen-js/examen.js
@@ -0,0 +1,470 @@
+// Calcula el producto de dos números dados
+// Contexto
+// Este desafío evalúa tu comprensión básica de las funciones.
+
+// Requerimientos
+// Crea una función product que reciba dos números y calcule su producto.
+
+// Ejemplo de Input/Output:
+// input: 2, 3
+// output: 6
+
+// input: 5, -5
+// output: -25
+
+// input: 4, 0
+// output: 0
+
+// Tu código y envío
+// Todo tu trabajo debe hacerse en la sección Solution Code. Para ver cómo se utilizará la función, verifica las pruebas proporcionadas en la sección Sample Tests. (Las pruebas son muy similares a las que ya has visto en los laboratorios.)
+
+// Consejo: Siempre querrás referirte a las pruebas ya que puede haber algunos casos especiales indicados en las pruebas que no sean demasiado obvios a partir de las instrucciones.
+
+// Envío
+// Mientras realizas el desafío, puedes enviar tu progreso varias veces en el botón Submit Solution.
+
+// Para hacer una presentación final del desafío, debes ir a la opción Review Assessment en la esquina superior derecha. Allí se te pedirá que revises tu código una vez más y hagas una presentación final. No puedes hacer ninguna nueva presentación después de que se haya completado este paso.
+
+// ¡Buena suerte!
+
+// Tu equipo de Ironhack
+
+
+
+// function product(x, y) {
+//     return x * y 
+//   }
+  
+  
+//   product(2,3)
+
+
+
+//   Comprobar si un número es par
+// Contexto
+// Este desafío prueba su comprensión básica de los tipos de datos, las condicionales y las funciones.
+
+// Requisitos
+// Cree una función llamada isEven que reciba un número como argumento. La función debe comprobar el número y devolver true si es par o false si no lo es.
+
+// Ejemplo de Input/Output:
+// input: 4
+// output: true
+
+// input: 3
+// output: false
+
+// Tu código y envío
+// Todo tu trabajo debe hacerse en la sección Solution Code. Para ver cómo se utilizará la función, verifica las pruebas proporcionadas en la sección Sample Tests. (Las pruebas son muy similares a las que ya has visto en los laboratorios.)
+
+// Consejo: Siempre querrás referirte a las pruebas ya que puede haber algunos casos especiales indicados en las pruebas que no sean demasiado obvios a partir de las instrucciones.
+
+// Envío
+// Mientras realizas el desafío, puedes enviar tu progreso varias veces en el botón Submit Solution.
+
+// Para hacer una presentación final del desafío, debes ir a la opción Review Assessment en la esquina superior derecha. Allí se te pedirá que revises tu código una vez más y hagas una presentación final. No puedes hacer ninguna nueva presentación después de que se haya completado este paso.
+
+// ¡Buena suerte!
+
+// Tu equipo de Ironhack
+
+
+// function isEven(num) {
+//     return (num % 2) == 0
+//    }
+   
+//    isEven(7)
+
+
+
+//    Encontrar el máximo de dos números
+// Contexto
+// Este desafío prueba su comprensión básica de los operadores de comparación, las condiciones y las funciones.
+
+// Requisitos
+// Cree una función llamada maxOfTwoNumbers que reciba cualquier par de números como argumentos. La función debe comparar dos números y devolver el número mayor.
+
+// Ejemplo de Input/Output:
+// input: 11, 19
+// output: 19
+
+// input: -7, -55
+// output: -7
+
+// Tu código y envío
+// Todo tu trabajo debe hacerse en la sección Solution Code. Para ver cómo se utilizará la función, verifica las pruebas proporcionadas en la sección Sample Tests. (Las pruebas son muy similares a las que ya has visto en los laboratorios.)
+
+// Consejo: Siempre querrás referirte a las pruebas ya que puede haber algunos casos especiales indicados en las pruebas que no sean demasiado obvios a partir de las instrucciones.
+
+// Envío
+// Mientras realizas el desafío, puedes enviar tu progreso varias veces en el botón Submit Solution.
+
+// Para hacer una presentación final del desafío, debes ir a la opción Review Assessment en la esquina superior derecha. Allí se te pedirá que revises tu código una vez más y hagas una presentación final. No puedes hacer ninguna nueva presentación después de que se haya completado este paso.
+
+// ¡Buena suerte!
+
+// Tu equipo de Ironhack
+
+
+
+// function maxOfTwoNumbers(a, b) {
+//     if(a > b) {
+//       return a
+//     }else return b
+//   }
+  
+//   maxOfTwoNumbers(8,5)
+
+
+
+
+//   Encontrar el máximo de tres números
+//   Contexto
+//   Este desafío pone a prueba su comprensión básica de los tipos de datos, las condiciones y las funciones.
+  
+//   Requisitos
+//   Cree una función llamada maxOfThreeNumbers que reciba cualquier tres números como argumentos. La función debe devolver el número más grande de los tres números proporcionados.
+  
+//   Ejemplo de Input/Output:
+//   input: 19, 88, 12
+//   output: 88
+  
+//   input: 12, -3, 0
+//   output: 12
+  
+//   input: -7, -55, -3
+//   output: -3
+  
+//   Tu código y envío
+//   Todo tu trabajo debe hacerse en la sección Solution Code. Para ver cómo se utilizará la función, verifica las pruebas proporcionadas en la sección Sample Tests. (Las pruebas son muy similares a las que ya has visto en los laboratorios.)
+  
+//   Consejo: Siempre querrás referirte a las pruebas ya que puede haber algunos casos especiales indicados en las pruebas que no sean demasiado obvios a partir de las instrucciones.
+  
+//   Envío
+//   Mientras realizas el desafío, puedes enviar tu progreso varias veces en el botón Submit Solution.
+  
+//   Para hacer una presentación final del desafío, debes ir a la opción Review Assessment en la esquina superior derecha. Allí se te pedirá que revises tu código una vez más y hagas una presentación final. No puedes hacer ninguna nueva presentación después de que se haya completado este paso.
+  
+//   ¡Buena suerte!
+  
+//   Tu equipo de Ironhack
+
+
+//   function maxOfThreeNumbers(a, b, c)  {
+//     max_val = 0;
+//     if (a > b){
+//       max_val = a;
+//     } else{
+//       max_val = b;
+//     } 
+    
+//     if (c > max_val) {
+//       max_val = c;
+//     } return max_val;
+//   }
+  
+  
+//   maxOfThreeNumbers(3, 5, 1)
+
+
+
+
+//   Calcular la suma de una matriz de números
+// Contexto
+// Este desafío prueba su comprensión básica de los tipos de datos, condicionales, bucles y funciones.
+
+// Requisitos
+// Cree una función llamada sumArray que reciba una matriz de números como argumento. La función debe devolver la suma de todos los números en la matriz.
+
+// Ejemplo de Input/Output:
+// input: [1, 2, 3, 4]
+// output: 10
+
+// input: [1, 2, 3, -4]
+// output: 2
+
+// input: [10, 5, 15]
+// output: 30
+
+// Tu código y envío
+// Todo tu trabajo debe hacerse en la sección Solution Code. Para ver cómo se utilizará la función, verifica las pruebas proporcionadas en la sección Sample Tests. (Las pruebas son muy similares a las que ya has visto en los laboratorios.)
+
+// Consejo: Siempre querrás referirte a las pruebas ya que puede haber algunos casos especiales indicados en las pruebas que no sean demasiado obvios a partir de las instrucciones.
+
+// Envío
+// Mientras realizas el desafío, puedes enviar tu progreso varias veces en el botón Submit Solution.
+
+// Para hacer una presentación final del desafío, debes ir a la opción Review Assessment en la esquina superior derecha. Allí se te pedirá que revises tu código una vez más y hagas una presentación final. No puedes hacer ninguna nueva presentación después de que se haya completado este paso.
+
+// ¡Buena suerte!
+
+// Tu equipo de Ironhack
+
+
+
+// function sumArray(numbers) {
+//     let suma = 0;
+//     if (numbers.length === 0) return 0;
+//     for (let i = 0; i < numbers.length; i++) {
+//       const numeroArray = numbers[i];
+//       suma += numeroArray;
+//     }
+//     return suma;
+//   }
+//    let numeros = [1,2,3,8,9,4,5]
+//    sumArray(numeros)
+
+
+
+
+//    Encontrar el máximo en un array de números
+// Contexto
+// Este desafío prueba su comprensión básica de los tipos de datos, condicionales, bucles y funciones.
+
+// Requisitos
+// Cree una función llamada maxOfArray que reciba un array de números como argumento. La función debe devolver el número más grande de los números proporcionados en el array.
+
+// Ejemplo de Input/Output:
+// input: [1, 2, 3, 4]
+// output: 4
+
+// input: [1, 2, 3, -4]
+// output: 3
+
+// Tu código y envío
+// Todo tu trabajo debe hacerse en la sección Solution Code. Para ver cómo se utilizará la función, verifica las pruebas proporcionadas en la sección Sample Tests. (Las pruebas son muy similares a las que ya has visto en los laboratorios.)
+
+// Consejo: Siempre querrás referirte a las pruebas ya que puede haber algunos casos especiales indicados en las pruebas que no sean demasiado obvios a partir de las instrucciones.
+
+// Envío
+// Mientras realizas el desafío, puedes enviar tu progreso varias veces en el botón Submit Solution.
+
+// Para hacer una presentación final del desafío, debes ir a la opción Review Assessment en la esquina superior derecha. Allí se te pedirá que revises tu código una vez más y hagas una presentación final. No puedes hacer ninguna nueva presentación después de que se haya completado este paso.
+
+// ¡Buena suerte!
+
+// Tu equipo de Ironhack
+
+
+
+// function maxOfArray(numbers) {
+//     if(numbers.length === 0){
+//       return false
+//     }else
+//    return Math.max(...numbers)
+//   }
+  
+  
+//   maxOfArray([1,5,8,9,6,4])
+
+
+
+
+//   Encontrar la cadena más larga
+// Contexto
+// Este desafío prueba su comprensión básica de los tipos de datos, condicionales, bucles y funciones.
+
+// Requisitos
+// Crea una función llamada longestString que reciba como argumento un array de cadenas. La función debe devolver la cadena más larga del array de cadenas proporcionado. Si el array contiene más de una cadena de la misma longitud, se debe devolver la primera cadena más larga encontrada.
+
+// Ejemplo de Input/Output:
+// input: ['ana', 'ivan', 'jan', 'nick']
+// output: 'ivan'
+
+// input: ['apple', 'pear', 'strawberries', 'mango']
+// output: 'strawberries'
+
+// input: ['foo', 'bar', 'baz']
+// output: 'foo'
+
+// Tu código y envío
+// Todo tu trabajo debe hacerse en la sección Solution Code. Para ver cómo se utilizará la función, verifica las pruebas proporcionadas en la sección Sample Tests. (Las pruebas son muy similares a las que ya has visto en los laboratorios.)
+
+// Consejo: Siempre querrás referirte a las pruebas ya que puede haber algunos casos especiales indicados en las pruebas que no sean demasiado obvios a partir de las instrucciones.
+
+// Envío
+// Mientras realizas el desafío, puedes enviar tu progreso varias veces en el botón Submit Solution.
+
+// Para hacer una presentación final del desafío, debes ir a la opción Review Assessment en la esquina superior derecha. Allí se te pedirá que revises tu código una vez más y hagas una presentación final. No puedes hacer ninguna nueva presentación después de que se haya completado este paso.
+
+// ¡Buena suerte!
+
+// Tu equipo de Ironhack
+
+
+
+// function longestString(strings) {
+//     let longestWord = "";
+  
+//     strings.forEach((word) => {
+//       if(word.length > longestWord.length) {
+//         longestWord = word;
+//       }
+//     });
+  
+//     return longestWord;
+//   }
+  
+  
+//   const arr = ['ana', 'ivan', 'jan', 'nick']
+  
+  
+//   longestString(arr)
+
+
+
+
+//   Obtener el nombre completo
+// Contexto
+// Este desafío prueba su comprensión básica de los tipos de datos, condicionales, bucles y funciones.
+
+// Requisitos
+// Crear una función llamada getFullName que reciba un objeto como argumento.
+// La función debe devolver una cadena concatenada que contenga los valores de las propiedades firstName y lastName del objeto.
+// Más específicamente, la cadena devuelta debe representar el nombre completo de una persona según el objeto proporcionado que contiene el nombre y el apellido como propiedades. Ejemplo de objeto { firstName: 'Tony', lastName: 'Stark'}.
+
+// Ejemplo de Input/Output:
+// Ejemplo 1:
+
+// const webStudent = {
+//   firstName: 'Ana', 
+//   lastName: 'Alonso'
+// }
+// input: webStudent
+// output: 'Ana Alonso'
+
+// Ejemplo 2:
+
+// const dataStudent = {
+//   firstName: 'mike', 
+//   lastName: 'oliveira'
+// }
+// input: dataStudent
+// output: 'mike oliveira'
+
+
+
+// Tu código y envío
+// Todo tu trabajo debe hacerse en la sección Solution Code. Para ver cómo se utilizará la función, verifica las pruebas proporcionadas en la sección Sample Tests. (Las pruebas son muy similares a las que ya has visto en los laboratorios.)
+
+// Consejo: Siempre querrás referirte a las pruebas ya que puede haber algunos casos especiales indicados en las pruebas que no sean demasiado obvios a partir de las instrucciones.
+
+// Envío
+// Mientras realizas el desafío, puedes enviar tu progreso varias veces en el botón Submit Solution.
+
+// Para hacer una presentación final del desafío, debes ir a la opción Review Assessment en la esquina superior derecha. Allí se te pedirá que revises tu código una vez más y hagas una presentación final. No puedes hacer ninguna nueva presentación después de que se haya completado este paso.
+
+// ¡Buena suerte!
+
+// Tu equipo de Ironhack
+
+
+
+// function getFullName(personObj) {
+//     let fullname = personObj.firstName + " " + personObj.lastName;
+//     return fullname
+//   }
+  
+//   const webStudent = {
+//     firstName: 'Ana', 
+//     lastName: 'Alonso'
+//   }
+  
+//   getFullName(webStudent)   
+
+
+
+
+//   ¿Existe la palabra?
+// Contexto
+// Este desafío prueba su comprensión básica de los tipos de datos, condicionales, bucles y funciones.
+
+// Requisitos
+// Crea una función llamada doesWordExist que reciba dos argumentos:
+
+// un array de strings y
+// un string con la palabra que queremos comprobar si existe en ese array.
+// La función debe devolver true/false dependiendo de si la palabra existe en el array o no.
+
+// Ejemplo de Input/Output:
+// input: doesWordExist( ['ana', 'jan', 'nick'], 'jan' )
+// output: true
+
+// input: doesWordExist( ['apple', 'pear', 'strawberries', 'pineapple'], 'banana' )
+// output: false
+
+// Tu código y envío
+// Todo tu trabajo debe hacerse en la sección Solution Code. Para ver cómo se utilizará la función, verifica las pruebas proporcionadas en la sección Sample Tests. (Las pruebas son muy similares a las que ya has visto en los laboratorios.)
+
+// Consejo: Siempre querrás referirte a las pruebas ya que puede haber algunos casos especiales indicados en las pruebas que no sean demasiado obvios a partir de las instrucciones.
+
+// Envío
+// Mientras realizas el desafío, puedes enviar tu progreso varias veces en el botón Submit Solution.
+
+// Para hacer una presentación final del desafío, debes ir a la opción Review Assessment en la esquina superior derecha. Allí se te pedirá que revises tu código una vez más y hagas una presentación final. No puedes hacer ninguna nueva presentación después de que se haya completado este paso.
+
+// ¡Buena suerte!
+
+// Tu equipo de Ironhack
+
+
+
+
+// function doesWordExist(wordsArr, word) {
+//     if (wordsArr.length === 0) return null;
+//      for (let element of wordsArr){
+//        if (element === word) return true;
+//      }
+//      return false;
+//    }
+  
+//   const names = ['ana', 'jan', 'nick']
+//   doesWordExist( names , 'jan' )
+
+
+
+
+//   Encontrar palabra única
+// Contexto
+// Este desafío prueba su comprensión básica de los tipos de datos, condicionales, bucles y funciones.
+
+// Requisitos
+// Crear una función llamada findUnique que reciba como argumento un array de palabras. La función debe devolver la primera palabra única (que no se repite) encontrada.
+
+// Ejemplo de Input/Output:
+// input: ['ana', 'ivan', 'jan', 'ana', 'jan']
+// output: 'ivan'
+
+// input: ['fizz', 'foo', 'bar', 'bazz', 'fizz', 'bazz']
+// output: 'foo'
+
+// input: ['apple', 'pear', 'strawberries', 'pineapple']
+// output: 'apple'
+
+// Tu código y envío
+// Todo tu trabajo debe hacerse en la sección Solution Code. Para ver cómo se utilizará la función, verifica las pruebas proporcionadas en la sección Sample Tests. (Las pruebas son muy similares a las que ya has visto en los laboratorios.)
+
+// Consejo: Siempre querrás referirte a las pruebas ya que puede haber algunos casos especiales indicados en las pruebas que no sean demasiado obvios a partir de las instrucciones.
+
+// Envío
+// Mientras realizas el desafío, puedes enviar tu progreso varias veces en el botón Submit Solution.
+
+// Para hacer una presentación final del desafío, debes ir a la opción Review Assessment en la esquina superior derecha. Allí se te pedirá que revises tu código una vez más y hagas una presentación final. No puedes hacer ninguna nueva presentación después de que se haya completado este paso.
+
+// ¡Buena suerte!
+
+// Tu equipo de Ironhack
+
+
+
+// function findUnique(wordsArr) {
+//     if (wordsArr.length === 0) {
+//       return false;
+//     }
+  
+//     for (let i = 0; i < wordsArr.length; i++) {
+//       const word = wordsArr[i];
+  
+//       if (wordsArr.indexOf(word) === wordsArr.lastIndexOf(word)) {
+//         return word;
+//       }
--- ih-ejercicios/js-ejercicios/methodos-array/index.js
+++ ih-ejercicios/js-ejercicios/methodos-array/index.js
@@ -1,103 +1,110 @@
-// --------- Ejercicio 1 (map): ---------
-// Crea un array con los nombres de 5 de tus amigos o familiares. Utiliza el método .map() para crear un nuevo array con las iniciales de cada nombre.
+// // --------- Ejercicio 1 (map): ---------
+// // Crea un array con los nombres de 5 de tus amigos o familiares. Utiliza el método .map() para crear un nuevo array con las iniciales de cada nombre.
 
-let nombre = ["Diego", "Facundo", "Eduardo"];
+// let nombre = ["Diego", "Facundo", "Eduardo"];
 
-let mapped = nombre.map((arrayElement) => arrayElement[0]);
+// let mapped = nombre.map((arrayElement) => arrayElement[0]);
 
-console.log(mapped);
+// console.log(mapped);
 
-// --------- Ejercicio 2 (filter): ---------
-//  Crea un array con números del 1 al 10. Utiliza el método .filter() para crear un nuevo array con solo los números pares.
+// // --------- Ejercicio 2 (filter): ---------
+// //  Crea un array con números del 1 al 10. Utiliza el método .filter() para crear un nuevo array con solo los números pares.
 
-let numeros = [1, 2, 3, 5, 6];
+// let numeros = [1, 2, 3, 5, 6];
 
-const metodofilter = numeros.filter((arrayElement) => {
-  return arrayElement % 2 == 0;
-});
+// const metodofilter = numeros.filter((arrayElement) => {
+//   return arrayElement % 2 == 0;
+// });
 
-console.log(metodofilter);
+// console.log(metodofilter);
 
-// --------- Ejercicio 3 (sort): ---------
-// Crea un array con nombres de frutas desordenadas. Utiliza el método .sort() para ordenar los nombres de las frutas en orden alfabético.
+// // --------- Ejercicio 3 (sort): ---------
+// // Crea un array con nombres de frutas desordenadas. Utiliza el método .sort() para ordenar los nombres de las frutas en orden alfabético.
 
-let alfabeto = ["Eduardo", "Facundo", "Diego"];
+// let alfabeto = ["Eduardo", "Facundo", "Diego"];
 
-let ordenAscendente = alfabeto.sort((a, b) => {
-  return a > b ? 1 : -1;
-});
+// let ordenAscendente = alfabeto.sort((a, b) => {
+//   return a > b ? 1 : -1;
+// });
 
-console.log(ordenAscendente);
+// console.log(ordenAscendente);
 
-// --------- Ejercicio 4 (forEach): ---------
-// Crea un array con números aleatorios. Utiliza el método .forEach() para ir sumando cada número a un resultado final.
+// // --------- Ejercicio 4 (forEach): ---------
+// // Crea un array con números aleatorios. Utiliza el método .forEach() para ir sumando cada número a un resultado final.
 
-let randomNum = [1, 2, 3, 5, 6];
+// let randomNum = [1, 2, 3, 5, 6];
 
-let total = 0;
+// let total = 0;
 
-randomNum.forEach((sumatoria) => {
-  return (total += sumatoria);
-});
-console.log(total);
+// randomNum.forEach((sumatoria) => {
+//   return (total += sumatoria);
+// });
+// console.log(total);
 
-// --------- Ejercicio 5 (concat): ---------
-// Crea dos arrays, uno con números y otro con letras. Utiliza el método .concat() para unir ambos arrays en un solo array.
+// // --------- Ejercicio 5 (concat): ---------
+// // Crea dos arrays, uno con números y otro con letras. Utiliza el método .concat() para unir ambos arrays en un solo array.
 
-const paises = ["Holanda", "Suecia", "Dinamarca", "Alemania"];
-const ciudades = ["Buenos Aires", "Barcelona", "New York", "Lima"];
+// const paises = ["Holanda", "Suecia", "Dinamarca", "Alemania"];
+// const ciudades = ["Buenos Aires", "Barcelona", "New York", "Lima"];
 
-const ciudadesMasPaises = paises.concat(ciudades);
+// const ciudadesMasPaises = paises.concat(ciudades);
 
-console.log(ciudadesMasPaises);
+// console.log(ciudadesMasPaises);
 
-// --------- Ejercicio 6 (every): ---------
-// Crea dos arrays con números. Utiliza el método .every() para verificar si todos los números del array son inferiores a 40. Haz que en un array devuelva true y en el otro false.
+// // --------- Ejercicio 6 (every): ---------
+// // Crea dos arrays con números. Utiliza el método .every() para verificar si todos los números del array son inferiores a 40. Haz que en un array devuelva true y en el otro false.
 
-const numerosEveryMore = [25, 50, 80, 10, 90, 35];
-const numerosEveryLess = [10, 20, 35, 2, 8, 15, 6];
+// const numerosEveryMore = [25, 50, 80, 10, 90, 35];
+// const numerosEveryLess = [10, 20, 35, 2, 8, 15, 6];
 
-let numerosEveryFalse = numerosEveryMore.every((number) => {
-  return number < 40;
-});
+// let numerosEveryFalse = numerosEveryMore.every((number) => {
+//   return number < 40;
+// });
 
-console.log(numerosEveryFalse);
+// console.log(numerosEveryFalse);
 
-let numerosEveryTrue = numerosEveryLess.every((number) => {
-  return number < 40;
-});
+// let numerosEveryTrue = numerosEveryLess.every((number) => {
+//   return number < 40;
+// });
 
-console.log(numerosEveryTrue);
+// console.log(numerosEveryTrue);
 
-// --------- Ejercicio 7 (some): ---------
-// Crea un array con objetos, cada objeto debe tener una propiedad llamada "status" con un valor "completo" o "incompleto". Utiliza el método .some() para verificar si al menos un objeto en el array tiene un status de "incompleto".
+// // --------- Ejercicio 7 (some): ---------
+// // Crea un array con objetos, cada objeto debe tener una propiedad llamada "status" con un valor "completo" o "incompleto". Utiliza el método .some() para verificar si al menos un objeto en el array tiene un status de "incompleto".
 
-// --------- Ejercicio 8 (includes): ---------
-// Crea un array con nombres de animales. Utiliza el método .includes() para verificar si un animal específic0 está en el array.
+// // --------- Ejercicio 8 (includes): ---------
+// // Crea un array con nombres de animales. Utiliza el método .includes() para verificar si un animal específic0 está en el array.
 
-// --------- Ejercicio 9 (join): ---------
-// Crea un array con nombres de animales. Utiliza el método .join() para convertir el array en un string separado por comas.
+// // --------- Ejercicio 9 (join): ---------
+// // Crea un array con nombres de animales. Utiliza el método .join() para convertir el array en un string separado por comas.
 
-// --------- Ejercicio 10 (push, pop, shift, unshift): ---------
-// Crea un array con nombres propios. Utiliza el método .pop() para eliminar el último nombre del array, .push() para añadir un nombre nuevo al final, .shift() para eliminar el primer elemento del array y .unshift() para añadir un nuevo nombre al inicio.
+// // --------- Ejercicio 10 (push, pop, shift, unshift): ---------
+// // Crea un array con nombres propios. Utiliza el método .pop() para eliminar el último nombre del array, .push() para añadir un nombre nuevo al final, .shift() para eliminar el primer elemento del array y .unshift() para añadir un nuevo nombre al inicio.
 
-// --------- Ejercicio 11 (slice): ---------
-// Crea un array con números del 1 al 10. Utiliza el método .slice() para crear un array que contenga solo del 5 al 8.
+// // --------- Ejercicio 11 (slice): ---------
+// // Crea un array con números del 1 al 10. Utiliza el método .slice() para crear un array que contenga solo del 5 al 8.
 
-// --------- Ejercicio 12 (reverse): ---------
-// Crea un array con strings. Utiliza el método .reverse() para invertir el orden de los strings en el array.
+// // --------- Ejercicio 12 (reverse): ---------
+// // Crea un array con strings. Utiliza el método .reverse() para invertir el orden de los strings en el array.
 
-// --------- Ejercicio 13 (find): ---------
-// Crea un array con nombres de cantantes. Utiliza el método .find() para encontrar el primer nombre que empiece por 'M'.
+// // --------- Ejercicio 13 (find): ---------
+// // Crea un array con nombres de cantantes. Utiliza el método .find() para encontrar el primer nombre que empiece por 'M'.
 
-// --------- Ejercicio 14 (findIndex): ---------
-// Crea un array de títulos de películas. Utiliza el método .findIndex() para encontrar el índice de la primera película cuyo título conste de solo una palabra.
+// // --------- Ejercicio 14 (findIndex): ---------
+// // Crea un array de títulos de películas. Utiliza el método .findIndex() para encontrar el índice de la primera película cuyo título conste de solo una palabra.
 
-// --------- Ejercicio 13 (indexOf): ---------
-// Crea un array con números. Utiliza el método .indexOf() para encontrar la primera aparición de un número específico en el array.
+// // --------- Ejercicio 13 (indexOf): ---------
+// // Crea un array con números. Utiliza el método .indexOf() para encontrar la primera aparición de un número específico en el array.
 
-// --------- Ejercicio 14 (fill): ---------
-// Crea un array con números aleatorios. Utilizando el método .fill(), haz que el cuarto elemento del array cambie su valor a 55.
+// // --------- Ejercicio 14 (fill): ---------
+// // Crea un array con números aleatorios. Utilizando el método .fill(), haz que el cuarto elemento del array cambie su valor a 55.
 
-// --------- Ejercicio 15 (reduce): ---------
-// Crea un array con números del 1 al 10. Utiliza el método .reduce() para sumar todos los números en el array.
+// // --------- Ejercicio 15 (reduce): ---------
+// // Crea un array con números del 1 al 10. Utiliza el método .reduce() para sumar todos los números en el array.
+function maxOfTwoNumbers(a, b) {
+  if(a > b) {
+    return a
+  }else return b
+}
+
+maxOfTwoNumbers(10,9)
