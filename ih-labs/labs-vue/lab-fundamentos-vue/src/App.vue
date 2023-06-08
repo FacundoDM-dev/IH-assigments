fd3d8262a2edd16ae93e03b14317659748d97964
FacundoDM-dev <delmedicofacundomartin@gmail.com>
2023-06-08 17:21:10 +0200

    lab-fundamentos-vue iteracion 1s

--- ih-labs/labs-vue/lab-fundamentos-vue/src/App.vue
+++ ih-labs/labs-vue/lab-fundamentos-vue/src/App.vue
@@ -1,8 +1,13 @@
 <script setup>
-
+import NavBarApp from './components/nav-bar-app.vue';
+import FooterApp from './components/footer-app.vue';
 </script>
 
 <template>
+
+<NavBarApp/>
+
+<FooterApp/>
 
 </template>
 
