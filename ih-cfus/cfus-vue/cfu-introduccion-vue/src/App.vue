0bd7d5ce5acea27d0894202faf4fcf88924cd590
FacundoDM-dev <delmedicofacundomartin@gmail.com>
2023-06-09 15:48:02 +0200

    cfu-introduccion-vue

---
+++ ih-cfus/cfus-vue/cfu-introduccion-vue/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ShortBiography from './components/ShortBiography.vue';
+</script>
+
+<template>
+<h1>Hello World</h1>
+<ShortBiography/>
+</template>
+
+<style scoped>
+
+</style>
--- ih-labs/labs-vue/lab-fundamentos-vue/src/App.vue
+++ ih-labs/labs-vue/lab-fundamentos-vue/src/App.vue
@@ -1,16 +1,33 @@
 <script setup>
 import NavBarApp from './components/nav-bar-app.vue';
 import FooterApp from './components/footer-app.vue';
+import HeaderAppHome from './components/header-app.vue';
+
+import { toRefs, reactive, defineComponent } from "vue";
+const state = reactive({
+  count: 0,
+});
+const increaseCount = () => {
+  state.count++;
+};
+
 </script>
 
 <template>
 
 <NavBarApp/>
 
+<HeaderAppHome/>
+
 <FooterApp/>
+
+<button @click="increaseCount">count is: {{ state.count }}</button>
 
 </template>
 
 <style scoped>
 
 </style>
+
+
+
