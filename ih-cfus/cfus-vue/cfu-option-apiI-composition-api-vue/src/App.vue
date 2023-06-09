bcbdd36424a6ddab5a89953677800096bd5f1eab
FacundoDM-dev <delmedicofacundomartin@gmail.com>
2023-06-09 16:03:57 +0200

    cfu-introduccion-vue

---
+++ ih-cfus/cfus-vue/cfu-option-apiI-composition-api-vue/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Options from "./components/options.vue";
+import Composition from "./components/composition.vue";
+</script>
+
+<template>
+  <Options />
+  <Composition />
+</template>
+
+<style scoped></style>
